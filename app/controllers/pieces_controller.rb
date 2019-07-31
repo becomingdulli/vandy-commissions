e096d1550ec62a59f897c12c09cadbc56c2b8a1f
Jason Miley <becomingdulli@gmail.com>
2019-07-31 01:05:13 +0000

    simple form addition

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -3,5 +3,18 @@ class PiecesController < ApplicationController
   def index
     @pieces = Piece.all
   end
+  def create
+     @piece = Piece.create(piece_params)
+      if @piece.invalid?
+        flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
+      end
+      redirect_to root_path
+  end
 
+
+private
+
+  def piece_params
+    params.require(:piece).permit(:title, :name, :description)
+  end
 end
